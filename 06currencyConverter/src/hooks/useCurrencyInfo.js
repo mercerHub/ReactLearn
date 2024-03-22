295774dbd767f57fe751a2a24c92490d95ac901c
Lalit <lalitkumarsingh3716@gmail.com>
2024-03-22 21:07:50 +0530

    fix: Updating data state on fetching api.

--- 06currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,18 +1,19 @@
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency){
-    const [data,setData] = useState({})
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
 
-    useEffect(() => {
-        let url = `https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`
+  useEffect(() => {
+    let url = `https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`;
 
-        fetch(url)
-            .then((res) => res.json())
-            .then((res) => res[currency])
-            console.log(data)
-    },[currency])
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res[currency]);
+      });
     console.log(data);
-    return data
+  }, [currency]);
+  return data;
 }
 
 export default useCurrencyInfo;
